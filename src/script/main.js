0e0d055dd42231e0778a1df0e99fd0fa1207aed1
MRDGH2821 <mihirrabade@gmail.com>
2022-11-17 18:45:43 +0530

    feat: add form listener & error reporting

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,18 @@
 // import * as pako from "https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js";
 
+const inputForm = document.getElementById("factorio-form");
+const bpForm = new FormData(inputForm);
+
+const inputBPStr = document.getElementById("raw-bp");
+const inputConfigStr = document.getElementById("config-json");
+
+const inputBPFile = document.getElementById("raw-bp-file");
+const inputConfigFile = document.getElementById("config-json-file");
+
+const errorElement = document.getElementById("errors");
+
 function decode() {
-  alert('decoding')
+  alert("decoding");
   try {
     const bpForm = new FormData(document.querySelector("factorio-form"));
     console.log(bpForm);
@@ -11,3 +22,21 @@ function decode() {
     console.log(err);
   }
 }
+
+inputForm.addEventListener("submit", (e) => {
+  console.log({ inputForm, bpForm, inputBPFile, inputConfigFile });
+  const messages = [];
+  if (inputBPStr.value === "" || inputBPStr.value === null || !!inputBPFile.files[0]) {
+    console.log("no bp found");
+    messages.push("blueprint");
+  }
+
+  if (inputConfigStr.value === "" || inputConfigStr.value === null || !!inputConfigFile.files[0]) {
+    console.log("no config found");
+    messages.push("config");
+  }
+  if (messages.length > 0) {
+    errorElement.innerText = "Please supply " + messages.join(" & ");
+  }
+  e.preventDefault();
+});
