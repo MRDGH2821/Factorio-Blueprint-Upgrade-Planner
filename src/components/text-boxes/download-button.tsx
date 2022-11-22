b93bace84a3a87b8cae1e2da9170379b89cf6b6f
MRDGH2821 <mihirrabade@gmail.com>
2022-11-22 18:46:00 +0530

    feat: add download button

--- src/components/text-boxes/download-button.tsx
+++ src/components/text-boxes/download-button.tsx
@@ -1,30 +1,59 @@
+import { component$, useContext } from '@builder.io/qwik';
+import { saveAs } from 'file-saver';
 import { deflate } from 'pako';
+import { BlueprintInputStore } from '../ts-interfaces/blueprint';
 import { BluePrintConfig, ConfigInputStore } from '../ts-interfaces/config';
+import { BPContext } from './blueprint-input';
+import { configContext } from './config-input';
+
+export default component$(() => {
+  const bpCtx = useContext(BPContext);
+  const cfgCtx = useContext(configContext);
+  return (
+    <>
+      <button
+        preventdefault:click
+        onClick$={() => {
+          const bp = generateFile(cfgCtx, bpCtx);
+          saveAs(bp, `Converted blueprint ${new Date().toUTCString}.txt`);
+        }}>
+        Download!
+      </button>
+    </>
+  );
+});
 
 export function replaceEntities(config: BluePrintConfig, blueprint: string) {
-    const copy = blueprint.slice();
-    for (const entity of config.entities) {
-      copy.replace(new RegExp(entity.old), entity.new);
-    }
-    return copy;
+  const copy = blueprint.slice();
+  for (const entity of config.entities) {
+    copy.replace(new RegExp(entity.old), entity.new);
   }
-  
-  export function bpCompress(blueprint: string) {
-    const bpDeflateStr = deflate(blueprint);
-    const bpBase64Str = btoa(bpDeflateStr.toString());
-    return bpBase64Str;
+  return copy;
+}
+
+export function bpCompress(blueprint: string) {
+  const bpDeflateStr = deflate(blueprint);
+  const bpBase64Str = btoa(bpDeflateStr.toString());
+  return bpBase64Str;
+}
+
+export function BlueprintConverter(cfgCtx: ConfigInputStore, bpCtx: BlueprintInputStore) {
+  console.log(cfgCtx);
+  const cfgObj = JSON.parse(cfgCtx.config as string) as BluePrintConfig;
+  try {
+    const transformedBlueprint = replaceEntities(cfgObj, bpCtx.decodedInput);
+    return bpCompress(transformedBlueprint);
+  } catch (error) {
+    const errTxt = `${
+      error || 'An error ocurred while converting. Please check the configuration provided.'
+    }`;
+    cfgCtx.errors = errTxt;
+    return errTxt;
   }
-  
-  export function BlueprintConverter(cfgCtx: ConfigInputStore) {
-    console.log(cfgCtx);
-    const cfgObj = JSON.parse(cfgCtx.config as string) as BluePrintConfig;
-  
-    try {
-      cfgCtx.transformedBlueprint = replaceEntities(cfgObj, cfgCtx.ogBP!.decodedInput);
-      cfgCtx.importableBlueprint = bpCompress(cfgCtx.transformedBlueprint);
-    } catch (error) {
-      cfgCtx.errors = `${
-        error || 'An error ocurred while converting. Please check the configuration provided.'
-      }`;
-    }
-  }
+}
+
+export function generateFile(cfgCtx: ConfigInputStore, bpCtx: BlueprintInputStore) {
+  const results = BlueprintConverter(cfgCtx, bpCtx);
+
+  return new Blob(results.split(''));
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,15 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 import manifest from '../../public/manifest.json';
 import BlueprintInput from '../components/text-boxes/blueprint-input';
 import ConfigInput from '../components/text-boxes/config-input';
-
+import DownloadButton from '../components/text-boxes/download-button';
 
 export default component$(() => {
   return (
     <div>
       <h1>Welcome to Factorio Blueprint Upgrade Planner!</h1>
-     <BlueprintInput/>
-     <ConfigInput/>
+      <BlueprintInput />
+      <ConfigInput />
+      <DownloadButton />
     </div>
   );
 });
