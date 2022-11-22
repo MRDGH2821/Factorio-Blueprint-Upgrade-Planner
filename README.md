f1b30b64ff9ade82a76edd7a22806c54ad09297f
MRDGH2821 <mihirrabade@gmail.com>
2022-11-22 22:44:57 +0530

    style: beautify files using prettier

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Factorio Blueprint Upgrade Planner
 
- Plan your upgrades in config, upload them in site, Profit!
+Plan your upgrades in config, upload them in site, Profit!
 
 ## Static Site Generator (Node.js)
 
--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -14,7 +14,6 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
 
       {head.meta.map((m) => (
         <meta {...m} />
