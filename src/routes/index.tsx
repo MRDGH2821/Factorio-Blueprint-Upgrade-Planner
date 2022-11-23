4214357a856cf5d5eecc82ad6ad570fe3c042028
MRDGH2821 <mihirrabade@gmail.com>
2022-11-23 16:26:30 +0530

    chore: use <p> tag to reduce clutter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -51,36 +51,29 @@ export default component$(() => {
     <div>
       <h1>Welcome to Factorio Blueprint Upgrade Planner!</h1>
       Use this site to quickly replace entities of one type to another with ease!
-      <br></br>
-      By default, this site uses <a href="../../public/default-config.json">
-        a minimal config
-      </a>{' '}
-      which converts belts, pipes & assemblers into Space versions from Space Exploration mod{' '}
-      <br></br>
-      Supports regex as well. Uses global flag, so no need to put any flags in regex string.
-      <br></br>
-      <br></br>
+      <p>
+        By default, this site uses <a href="../../public/default-config.json">a minimal config</a>{' '}
+        which converts belts, pipes & assemblers into Space versions from Space Exploration mod.
+        <br />
+        Supports regex as well. Uses global flag, so no need to put any flags in regex string.
+      </p>
       <BlueprintInput bpStrStore={bpStrStore} />
       <ConfigInput cfgStore={cfgStore} />
       <DownloadButton BPstore={bpStrStore} CFGstore={cfgStore} />
       <br></br>
-      <br></br>
       <h2>How to create your own config?</h2>
       Open Factorio's <a href="https://wiki.factorio.com/Debug_mode">Debug Mode</a> & enable{' '}
       <mark>show-debug-info-in-tooltips</mark> which will enable you to see entity's internal name.
-      <br></br>
-      Take a note of which entity you want to replace with which one.
-      <br></br>
-      For example, let's replace pipes. Regular pipes have internal name: <mark>pipe</mark>
-      <br></br>
-      Now to replace them with Space pipes which have internal name: <mark>se-space-pipe</mark>
-      <br></br>
-      Your resulting config should be in JSON. So our example config would look like this:
-      <br></br>
+      <p>
+        Take a note of which entity you want to replace with which one. <br />
+        For example, let's replace pipes. Regular pipes have internal name: <mark>pipe</mark>
+        <br />
+        Now to replace them with Space pipes which have internal name: <mark>se-space-pipe</mark>
+        <br />
+        Your resulting config should be in JSON. So our example config would look like this:
+      </p>
       <DisplayCode code={code1} />
-      <br></br>
-      <br></br>
-      Replacing belts with Space versions by using regex:
+      <p>Replacing belts with Space versions by using regex:</p>
       <DisplayCode code={code2} />
     </div>
   );
