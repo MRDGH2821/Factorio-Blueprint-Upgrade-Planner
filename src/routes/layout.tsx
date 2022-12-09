42572402368b656f2794aff0544af4076025e9a1
MRDGH2821 <mihirrabade@gmail.com>
2022-12-09 20:16:01 +0530

    chore: add source code link

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -8,7 +8,10 @@ export default component$(() => {
           <Slot />
         </section>
       </main>
-      <footer>Made by MRDGH2821</footer>
+      <footer>
+        Made by MRDGH2821 |{' '}
+        <a href="https://github.com/MRDGH2821/Factorio-Blueprint-Upgrade-Planner/">Source Code</a>
+      </footer>
     </>
   );
 });
